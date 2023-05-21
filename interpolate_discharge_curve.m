5e61360798bedb218ac471f9d3b0598ea3180f09
sdamkjar <sdamkjar@gmail.com>
2023-05-21 01:08:12 -0600

    Update interpolate_discharge_curve.m

--- interpolate_discharge_curve.m
+++ interpolate_discharge_curve.m
@@ -15,8 +15,9 @@
 % WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 % See the License for the specific language governing permissions and
 % limitations under the License.
-%
-%
+
+
+
 % This function accepts a discharge rate and a temperature as inputs and
 % creates a new discharge curve by interpolating between the available
 % curves based on the inputs.
